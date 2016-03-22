3943de83d05a4fd60727652670b1a973a356b266
hui32 <442970058@qq.com>
2016-03-22 11:19:41 +0800

    intent jump

--- app/src/main/java/liu/com/mydocuments/util/JumpUtil.java
+++ app/src/main/java/liu/com/mydocuments/util/JumpUtil.java
@@ -9,11 +9,11 @@ import android.os.Bundle;
  * Created by liuhui on 16/3/21.
  */
 public class JumpUtil {
-    public static void jumpTo(Context context,Class<? extends Activity> targetClass){
+    public static void jump(Context context,Class<? extends Activity> targetClass){
         Intent intent = new Intent(context,targetClass);
         context.startActivity(intent);
     }
-    public static void jumpToWithBoundle(Context context,Class<? extends Activity> targetClass,Bundle bundle){
+    public static void jumpWithBoundle(Context context,Class<? extends Activity> targetClass,Bundle bundle){
         Intent intent = new Intent(context,targetClass);
         intent.putExtras(bundle);
         context.startActivity(intent);
